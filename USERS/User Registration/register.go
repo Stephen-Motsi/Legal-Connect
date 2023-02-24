b1d929a1babfb3bb82f2cb855d4b9c15d3214c65
kariuki sophia <skwanjiku@kabarak.ac.ke>
2023-02-24 12:13:32 +0300

    refine user input options

--- USERS/User Registration/register.go
+++ USERS/User Registration/register.go
@@ -37,16 +37,44 @@ func registerUser(w http.ResponseWriter, r *http.Request) {
 	// Add the user to the users slice
 	users = append(users, newUser)
 
-	// Redirect the user to the dashboard or another page after successful registration
-	http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
-
 	// Send a response indicating success
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "User created successfully")
+
+	// Redirect the user to the dashboard or another page after successful registration
+	http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+}
+
+func loginUser(w http.ResponseWriter, r *http.Request) {
+	// Decode the request body to a User struct
+	var loginUser User
+	err := json.NewDecoder(r.Body).Decode(&loginUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Check if the user with the given email and password exists
+	for _, user := range users {
+		if user.Email == loginUser.Email && user.Password == loginUser.Password {
+			// Redirect the user to the dashboard or another page after successful login
+			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+			return
+		}
+	}
+
+	// If no matching user is found, send an HTTP response indicating failure
+	http.Error(w, "Invalid email or password", http.StatusUnauthorized)
 }
 
 func main() {
-	// Create a new HTTP server and register the registerUser handler function
-	http.HandleFunc("/users", registerUser)
+	// Create a new HTTP server and register the registerUser and loginUser handler functions
+	http.HandleFunc("/register", registerUser)
+	http.HandleFunc("/login", loginUser)
+
+	// Serve static files such as HTML, CSS, and JavaScript from the "homepage" directory
+	fs := http.FileServer(http.Dir("homepage"))
+	http.Handle("/", fs)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
